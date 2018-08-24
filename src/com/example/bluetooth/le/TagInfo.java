b42001f83951f7de0f18bc9ef349ae2e2c34b195
pangshanwei <11>
2018-08-25 02:07:30 +0800

    first commit

---
+++ src/com/example/bluetooth/le/TagInfo.java
@@ -0,0 +1,35 @@
+package com.example.bluetooth.le;
+
+public class TagInfo {
+
+	private String epc ;
+	private int rssi ;
+	private String pc ;
+	private int count ;
+	public String getEpc() {
+		return epc;
+	}
+	public void setEpc(String epc) {
+		this.epc = epc;
+	}
+	public int getRssi() {
+		return rssi;
+	}
+	public void setRssi(int rssi) {
+		this.rssi = rssi;
+	}
+	public String getPc() {
+		return pc;
+	}
+	public void setPc(String pc) {
+		this.pc = pc;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+}
---
+++ src/com/example/bluetooth/le/Tools.java
@@ -0,0 +1,91 @@
+package com.example.bluetooth.le;
+
+
+public class Tools {
+
+	//byte 转十六进制
+		public static String Bytes2HexString(byte[] b, int size) {
+		    String ret = "";
+		    for (int i = 0; i < size; i++) {
+		      String hex = Integer.toHexString(b[i] & 0xFF);
+		      if (hex.length() == 1) {
+		        hex = "0" + hex;
+		      }
+		      ret += hex.toUpperCase();
+		    }
+		    return ret;
+		  }
+		
+		public static byte uniteBytes(byte src0, byte src1) {
+		    byte _b0 = Byte.decode("0x" + new String(new byte[]{src0})).byteValue();
+		    _b0 = (byte)(_b0 << 4);
+		    byte _b1 = Byte.decode("0x" + new String(new byte[]{src1})).byteValue();
+		    byte ret = (byte)(_b0 ^ _b1);
+		    return ret;
+		  }
+		
+		//十六进制转byte
+		public static byte[] HexString2Bytes(String src) {
+			int len = src.length() / 2;
+			byte[] ret = new byte[len];
+			byte[] tmp = src.getBytes();
+
+			for (int i = 0; i < len; i++) {
+				ret[i] = uniteBytes(tmp[i * 2], tmp[i * 2 + 1]);
+			}
+			return ret;
+		}
+		
+		/* byte[]转Int */
+		public static int bytesToInt(byte[] bytes)
+		{
+			int addr = bytes[0] & 0xFF;
+			addr |= ((bytes[1] << 8) & 0xFF00);
+			addr |= ((bytes[2] << 16) & 0xFF0000);
+			addr |= ((bytes[3] << 25) & 0xFF000000);
+			return addr;
+
+		}
+
+		/* Int转byte[] */
+		public static byte[] intToByte(int i)
+		{
+			byte[] abyte0 = new byte[4];
+			abyte0[0] = (byte) (0xff & i);
+			abyte0[1] = (byte) ((0xff00 & i) >> 8);
+			abyte0[2] = (byte) ((0xff0000 & i) >> 16);
+			abyte0[3] = (byte) ((0xff000000 & i) >> 24);
+			return abyte0;
+		}
+		
+		
+	    public static String bin2hex(String bin) {
+	        char[] digital = "0123456789ABCDEF".toCharArray();
+	        StringBuffer sb = new StringBuffer("");
+	        byte[] bs = bin.getBytes();
+	        int bit;
+	        for (int i = 0; i < bs.length; i++) {
+	            bit = (bs[i] & 0x0f0) >> 4;
+	            sb.append(digital[bit]);
+	            bit = bs[i] & 0x0f;
+	            sb.append(digital[bit]);
+	        }
+	        return sb.toString();
+	    }
+	    public static byte[] hex2byte(byte[] b) {
+	        if ((b.length % 2) != 0) {
+	            throw new IllegalArgumentException("长度不是偶数");
+	        }
+	        byte[] b2 = new byte[b.length / 2];
+	        for (int n = 0; n < b.length; n += 2) {
+	            String item = new String(b, n, 2);
+	            // 两位一组，表示一个字节,把这样表示的16进制字符串，还原成一个进制字节
+	            b2[n / 2] = (byte) Integer.parseInt(item, 16);
+	        }
+	        b = null;
+	        return b2;
+	    }
+		
+		
+
+}
