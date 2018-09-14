4f6b0c859c58b91dad7b110b532b70df6ba06e43
pangshanwei <360372510@qq.com>
2018-09-15 01:02:54 +0800

    use fragment

---
+++ src/com/example/bluetooth/le/fragment/InventoryFragment.java
@@ -0,0 +1,21 @@
+package com.example.bluetooth.le.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * 盘存界面
+ * @author pangshanwei
+ *
+ */
+public class InventoryFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
